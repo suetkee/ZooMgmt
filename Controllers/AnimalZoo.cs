da0dcc0e75704a0e14f7819e134f6b4240585298
Suet Kee <suet.kee@softwire.com>
2025-03-11 13:34:05 +0000

    Added model

--- Controllers/AnimalZoo.cs
+++ Controllers/AnimalZoo.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace AnimalZoo.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("[controller]")] 
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
